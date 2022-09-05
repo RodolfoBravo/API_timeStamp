e2e37cf02051e9c12e088a554b5e419153d36408
Rodolfo Bravo Garcia <abravor5@adient.com>
2022-09-05 01:18:06 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,61 @@
+// index.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+
+app.get("/api/:dateUser", function (req, res) {
+  const { dateUser } = req.params;
+  var letras="abcdefghyjklmnñopqrstuvwxyz";
+  
+  function have_letters(text){
+   text = text.toLowerCase();
+   for(i=0; i<text.length; i++){
+      if (letras.indexOf(text.charAt(i),0)!=-1){
+         return 1;
+      }
+   }
+   return 0;
+}
+if(have_letters(dateUser) ==0)
+  {var date = new Date(dateUser); 
+  var dateGMT = date.toUTCString();
+  var dateUnix = date.getTime();
+  if (dateGMT === 'Invalid Date'){
+    date = new Date(parseInt(dateUser,10));
+    var dateGMT = date.toUTCString();
+    var dateUnix = date.getTime();
+  } 
+  var responde = {'unix':dateUnix,'utc':dateGMT};}
+  else {
+    var responde = {'error':'Invalid Date'}
+  }
+  res.json(responde);
+});
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
